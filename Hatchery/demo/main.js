d6e3b79b77af7e853f27c34c8bbd7e4ed8563aec
petercsengodi <petercsengodi@users.noreply.github.com>
2023-07-09 00:24:26 +0200

    Test custom geometry

--- Hatchery/demo/main.js
+++ Hatchery/demo/main.js
@@ -1,5 +1,25 @@
 import * as THREE from 'three';
 
+const geometry = new THREE.BufferGeometry();
+
+const vertices = new Float32Array( [
+	-1.0, -1.0,  1.0, // v0
+	 1.0, -1.0,  1.0, // v1
+	 1.0,  1.0,  1.0, // v2
+	-1.0,  1.0,  1.0, // v3
+] );
+
+const indices = [
+	0, 1, 2,
+	2, 3, 0,
+];
+
+geometry.setIndex( indices );
+geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+
+const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const mesh = new THREE.Mesh( geometry, material );
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -7,18 +27,21 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+/*
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+*/
+
+scene.add( mesh );
 
 camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
 }
