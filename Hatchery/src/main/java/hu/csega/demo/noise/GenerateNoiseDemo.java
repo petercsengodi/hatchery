e9d6d2e5df6372785fb721b79938c991ac3cd3c1
petercsengodi <petercsengodi@users.noreply.github.com>
2023-08-31 08:00:23 +0200

    Animate cube texture

--- Hatchery/src/main/java/hu/csega/demo/noise/GenerateNoiseDemo.java
+++ Hatchery/src/main/java/hu/csega/demo/noise/GenerateNoiseDemo.java
@@ -1,18 +1,8 @@
 package hu.csega.demo.noise;
 
 import hu.csega.editors.AnimatorStarter;
-import hu.csega.editors.anm.layer1.view3d.AnimatorSetPart;
-import hu.csega.editors.anm.layer4.data.model.AnimatorRefreshViews;
-import hu.csega.editors.common.SerializationUtil;
 import hu.csega.editors.common.resources.FileResourceAdapter;
 import hu.csega.editors.common.resources.ResourceAdapter;
-import hu.csega.editors.ftm.layer4.data.FreeTriangleMeshSnapshots;
-import hu.csega.games.library.animation.v1.anm.Animation;
-import hu.csega.games.library.animation.v1.anm.AnimationPart;
-import hu.csega.games.library.animation.v1.anm.AnimationPersistent;
-import hu.csega.games.library.mesh.v1.ftm.FreeTriangleMeshModel;
-import hu.csega.games.library.mesh.v1.ftm.FreeTriangleMeshTriangle;
-import hu.csega.games.library.mesh.v1.ftm.FreeTriangleMeshVertex;
 import hu.csega.toolshed.logging.Level;
 import hu.csega.toolshed.logging.Logger;
 import hu.csega.toolshed.logging.LoggerFactory;
@@ -25,14 +15,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-
-import org.joml.Matrix4f;
 
 public class GenerateNoiseDemo {
 
@@ -42,8 +24,6 @@ public class GenerateNoiseDemo {
     public static final String DEMO_PROJECT = "noise";
 
     private static ResourceAdapter resourceAdapter;
-
-    private static final Map<String, File> meshNameToFile = new LinkedHashMap<>();
 
     public static void main(String[] args) throws Exception {
         ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -77,46 +57,11 @@ public class GenerateNoiseDemo {
                 File.separator + "hu" + File.separator + "csega" + File.separator + "demo" + File.separator + DEMO_PROJECT +
                 File.separator + DEMO_PROJECT + ".tail.js");
 
-        File dragonAnm = new File(resourceAdapter.animationFolder() + File.separator + /* "dragon.anm" */ "run_2.anm");
-
         try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(mainJS))) {
-
             write(writer, head);
-
-            generateFromFtm2(writer, "Hat", new File(resourceAdapter.meshRoot() + "hat.ftm"));
-
-            generateFromAnm(writer, "Dragon", dragonAnm);
-
-            for(Map.Entry<String, File> entry : meshNameToFile.entrySet()) {
-                generateFromFtm(writer, entry.getKey(), entry.getValue());
-            }
-
-            for(Map.Entry<String, File> entry : meshNameToFile.entrySet()) {
-                addToParts(writer, entry.getKey());
-            }
 
             write(writer, tail);
         }
-    }
-
-    private static String pathToFilename(String filename) {
-        int lastIndex = filename.lastIndexOf(File.separator);
-        if(lastIndex >= 0 && lastIndex < filename.length() - 1) {
-            filename = filename.substring(lastIndex + 1);
-        }
-
-        lastIndex = filename.lastIndexOf('.');
-        if(lastIndex >= 1 && lastIndex < filename.length()) {
-            filename = filename.substring(0, lastIndex);
-        }
-
-        return filename;
-    }
-
-    private static void write(OutputStreamWriter writer, String lines) throws IOException {
-        writer.write(lines);
-        writer.write("\n");
-        writer.flush();
     }
 
     private static void write(OutputStreamWriter writer, InputStream stream) throws IOException {
@@ -133,169 +78,6 @@ public class GenerateNoiseDemo {
 
     private static void write(OutputStreamWriter writer, File file) throws IOException {
         write(writer, new FileInputStream(file));
-    }
-
-    private static void generateFromFtm(OutputStreamWriter writer, String name, File ftm) throws IOException {
-        byte[] bytes = FreeTriangleMeshSnapshots.readAllBytes(ftm);
-        FreeTriangleMeshModel deserialized = SerializationUtil.deserialize(bytes, FreeTriangleMeshModel.class);
-        logger.info(ftm.getAbsolutePath() + ": " + deserialized.getClass().getName());
-
-        writer.write("const geometry" + name + " = new THREE.BufferGeometry();\n\n");
-        writer.write("const vertices" + name + " = new Float32Array([\n");
-
-        List<FreeTriangleMeshVertex> vertices = deserialized.getVertices();
-        for(FreeTriangleMeshVertex vertex : vertices) {
-            writer.write("\t" + vertex.getPX() + ", " + vertex.getPY() + ",  " + vertex.getPZ()+ ",\n");
-        }
-
-        writer.write("]);\n\n");
-        writer.write("const indices" + name + " = [\n");
-
-        List<FreeTriangleMeshTriangle> triangles = deserialized.getTriangles();
-        for(FreeTriangleMeshTriangle triangle : triangles) {
-            writer.write("\t" + triangle.getVertex1() + ", " + triangle.getVertex2() + ", " + triangle.getVertex3() + ",\n");
-        }
-
-        writer.write("];\n\n");
-        writer.write("geometry" + name + ".setIndex(indices" + name + ");\n");
-        writer.write("geometry" + name + ".setAttribute('position', new THREE.BufferAttribute(vertices" + name + ", 3));\n");
-        writer.write("const material" + name + " = new THREE.MeshBasicMaterial({color: 0xff0000});\n");
-        writer.write("const mesh" + name + " = new THREE.Mesh(geometry" + name + ", material" + name + ");\n");
-        writer.write("mesh" + name + ".matrixAutoUpdate = false;\n\n");
-        writer.flush();
-    }
-
-    private static void generateFromFtm2(OutputStreamWriter writer, String name, File ftm) throws IOException {
-        byte[] bytes = FreeTriangleMeshSnapshots.readAllBytes(ftm);
-        FreeTriangleMeshModel deserialized = SerializationUtil.deserialize(bytes, FreeTriangleMeshModel.class);
-        logger.info(ftm.getAbsolutePath() + ": " + deserialized.getClass().getName());
-
-        writer.write("const geometry" + name + " = new THREE.BufferGeometry();\n\n");
-        writer.write("const vertices" + name + " = new Float32Array([\n");
-
-        List<FreeTriangleMeshVertex> vertices = deserialized.getVertices();
-        for(FreeTriangleMeshVertex vertex : vertices) {
-            writer.write("\t" + vertex.getPX() + ", " + vertex.getPY() + ",  " + vertex.getPZ()+ ",\n");
-        }
-
-        writer.write("]);\n\n");
-        writer.write("const indices" + name + " = [\n");
-
-        List<FreeTriangleMeshTriangle> triangles = deserialized.getTriangles();
-        for(FreeTriangleMeshTriangle triangle : triangles) {
-            writer.write("\t" + triangle.getVertex1() + ", " + triangle.getVertex2() + ", " + triangle.getVertex3() + ",\n");
-        }
-
-        writer.write("];\n\n");
-        writer.write("const colors" + name + " = [\n");
-
-        for(FreeTriangleMeshTriangle triangle : triangles) {
-            FreeTriangleMeshVertex v1 = vertices.get(triangle.getVertex1());
-            FreeTriangleMeshVertex v2 = vertices.get(triangle.getVertex2());
-            FreeTriangleMeshVertex v3 = vertices.get(triangle.getVertex3());
-
-            double x = (v1.getTX() + v2.getTX() + v3.getTX()) / 3.0;
-            double y = (v1.getTY() + v2.getTY() + v3.getTY()) / 3.0;
-            int colorIndex = (int)(Math.floor(9.0 * x) + Math.floor(9.0 * y) * 3);
-            writer.write("\t" + colorIndex + ",\n");
-        }
-
-        writer.write("];\n\n");
-        writer.flush();
-    }
-
-    private static void generateFromAnm(OutputStreamWriter writer, String name, File anm) throws IOException {
-        byte[] bytes = FreeTriangleMeshSnapshots.readAllBytes(anm);
-        AnimationPersistent deserialized = SerializationUtil.deserialize(bytes, AnimationPersistent.class);
-        logger.info(anm.getAbsolutePath() + ": " + deserialized.getClass().getName());
-
-        Animation animation = deserialized.getAnimation();
-        Map<String, AnimationPart> parts = animation.getParts();
-
-        final Map<String, Integer> idToIndex = new HashMap<>();
-        for(AnimationPart part : parts.values()) {
-            int index = meshNameToFile.size();
-
-            String filename = pathToFilename(part.getMesh());
-            File meshFile = new File(resourceAdapter.meshRoot() + filename + ".ftm");
-
-            String meshName = "Part" + index;
-            meshNameToFile.put(meshName, meshFile);
-
-            idToIndex.put(part.getIdentifier(), index);
-        }
-
-        int numberOfScenes = animation.getNumberOfScenes();
-        writer.write("const numberOfScenes" + name + " = " + numberOfScenes + ";\n");
-        writer.write("const animation" + name + " = [\n");
-
-        for(int sceneIndex = 0; sceneIndex < numberOfScenes; sceneIndex++) {
-            writer.write("\t[\n");
-
-            Matrix4f baseTransformation = new Matrix4f();
-            List<AnimatorSetPart> resultParts = new ArrayList<>();
-            AnimatorRefreshViews.generateParts(animation, sceneIndex, baseTransformation, resultParts);
-
-            Map<Integer, AnimatorSetPart> map = new TreeMap<>();
-            for(AnimatorSetPart part : resultParts) {
-                String id = part.getIdentifier();
-                Integer index = idToIndex.get(id);
-                if(index != null) {
-                    map.put(index, part);
-                }
-            }
-
-            for(int partIndex = 0; partIndex < idToIndex.size(); partIndex++) {
-                AnimatorSetPart part = map.get(partIndex);
-                if(part == null)
-                    continue;
-
-                float[] floats = part.getTransformation().getFloats();
-                writer.write("\t\tnew THREE.Matrix4(");
-
-                for(int i = 0; i < 16; i++) {
-                    if(i > 0) {
-                        writer.write(", ");
-                    }
-
-                    int row = i % 4;
-                    int col = i / 4;
-                    int j = row * 4 + col;
-
-                    writer.write(String.valueOf(floats[j]));
-                }
-
-
-                writer.write("),\n");
-                writer.flush();
-            }
-
-            writer.write("\t],\n");
-        }
-
-        writer.write("]; // end of animation" + name + "\n");
-        writer.flush();
-    }
-
-    private static void addToParts(OutputStreamWriter writer, String... names) throws IOException {
-        for(String name : names) {
-            writer.write("parts.push(mesh" + name + ");\n");
-            writer.write("verticesArray.push(vertices" + name + ");\n");
-            writer.write("indicesArray.push(indices" + name + ");\n");
-        }
-
-        writer.write("\n");
-        writer.flush();
-    }
-
-    private static void addToScene(OutputStreamWriter writer, String... names) throws IOException {
-        for(String name : names) {
-            writer.write("scene.add(mesh" + name + ");\n");
-            writer.write("parts.push(mesh" + name + ");\n");
-        }
-
-        writer.write("\n");
-        writer.flush();
     }
 
 }
--- Hatchery/src/main/java/hu/csega/demo/noise/noise.head.js
+++ Hatchery/src/main/java/hu/csega/demo/noise/noise.head.js
@@ -2,123 +2,135 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.x = 250;
-camera.position.y = 0;
-camera.position.z = -50;
+camera.position.x = 25;
+camera.position.y = 10;
+camera.position.z = -15;
 camera.lookAt(0, 0, 0);
-
-var startTime = Date.now();
-var cameraMovement = [];
-
-cameraMovement.push({
-    t: 0,
-    position: { x: 250, y: 0, z: -50},
-    lookAt : { x: 0, y: 0, z: 0}
-});
-
-cameraMovement.push({
-    t: 5000,
-    position: { x: -250, y: 0, z: -100},
-    lookAt : { x: 0, y: 0, z: 0}
-});
-
-cameraMovement.push({
-    t: 10000,
-    position: { x: -50, y: 100, z: -100},
-    lookAt : { x: 0, y: 0, z: 0}
-});
-
-cameraMovement.push({
-    t: 15000,
-    position: { x: 250, y: 0, z: -50},
-    lookAt : { x: 0, y: 0, z: 0}
-});
-
-cameraMovement.push({
-    t: 20000,
-    position: { x: 0, y: 0, z: 200},
-    lookAt : { x: 0, y: 0, z: 0}
-});
-
-cameraMovement.push({
-    t: 25000,
-    position: { x: -250, y: 0, z: -50},
-    lookAt : { x: 0, y: 0, z: 0}
-});
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var i, j, k;
-var colors = [ 0, 128, 255 ];
-
-var materials = [];
-for(i = 0; i < 3; i++) {
-    for(j = 0; j < 3; j++) {
-        for(k = 0; k < 3; k++) {
-            materials.push(new THREE.MeshBasicMaterial({
-                color: (colors[i] * 256 * 256 + colors[j] * 256 + colors[k]),
-                side: THREE.DoubleSide
-            }));
-        }
-    }
-}
-
-for(i = 0; i < 9; i++) {
-    materials.push(new THREE.MeshBasicMaterial({
-        color: ((25*(i+1)-10) *256 * 256 + (25*(i+1)-10) * 256 + 25*(i+1)-10),
-        side: THREE.DoubleSide
-    }));
-}
-
-var triangles = [];
-var triangleAnimations = [];
-var numberOfTriangles = 1000;
-var usedTriangleIndex = 0;
-
-for(i = 0; i < numberOfTriangles; i++) {
-    var geometry = new THREE.BufferGeometry();
-    var vertices = new Float32Array([ 1, 0, 0, 0, 1, 0, 0, 0, 1 ]);
-    var indices = [ 0, 1, 2 ];
-    geometry.setIndex(indices);
-    geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-    geometry.computeVertexNormals();
-    var mesh = new THREE.Mesh(geometry, materials[2]);
-    mesh.matrixAutoUpdate = false;
-    triangles.push(mesh);
-    scene.add(mesh);
-
-    var animation = {
-        source: [0,100,-200,0,100,-200,0,100,-200],
-        target: [0,100,-200,0,100,-200,0,100,-200],
-        delay: 0,
-        t: 0
+const canvasMod = {};
+function setup(api) {
+    //-------- ----------
+    // built in draw methods
+    //-------- ----------
+    const DRAW = {};
+    // square draw method
+    DRAW.square = (canObj, ctx, canvas, state) => {
+        ctx.fillStyle = canObj.palette[0]
+        ctx.lineWidth = 1;
+        ctx.fillRect(0.5, 0.5, canvas.width - 1, canvas.height - 1);
+        ctx.strokeStyle = canObj.palette[1]
+        ctx.strokeRect(0.5, 0.5, canvas.width - 1, canvas.height - 1);
     };
-
-    triangleAnimations.push(animation);
+    // random using palette colors
+    DRAW.rnd = (canObj, ctx, canvas, state) => {
+        let i = 0;
+        const gSize =  state.gSize === undefined ? 5 : state.gSize;
+        const len = gSize * gSize;
+        const pxSize = canObj.size / gSize;
+        while(i < len){
+            const ci = Math.floor( canObj.palette.length * Math.random() );
+            const x = i % gSize;
+            const y = Math.floor(i / gSize);
+            ctx.fillStyle = canObj.palette[ci];
+            ctx.fillRect(0.5 + x * pxSize, 0.5 + y * pxSize, pxSize, pxSize);
+            i += 1;
+        }
+    };
+    //-------- ----------
+    // HELEPRS
+    //-------- ----------
+    // parse draw option helper
+    const parseDrawOption = (opt) => {
+        // if opt.draw is false for any reason return DRAW.square
+        if(!opt.draw){
+            return DRAW.square;
+        }
+        // if a string is given assume it is a key for a built in draw method
+        if(typeof opt.draw === 'string'){
+            return DRAW[opt.draw];
+        }
+        // assume we where given a custom function
+        return opt.draw;
+    };
+    //-------- ----------
+    // PUBLIC API
+    //-------- ----------
+    // create and return a canvas texture
+    api.create = function (opt) {
+        opt = opt || {};
+        // create canvas, get context, set size
+        const canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d');
+        opt.size = opt.size === undefined ? 16 : opt.size;
+        canvas.width = opt.size;
+        canvas.height = opt.size;
+        // create canvas object
+        const canObj = {
+            texture: null,
+            size: opt.size,
+            canvas: canvas, ctx: ctx,
+            palette: opt.palette || ['black', 'white'],
+            state: opt.state || {},
+            draw: parseDrawOption(opt)
+        };
+        // create texture object
+        canObj.texture = new THREE.CanvasTexture(canvas);
+        canObj.texture.magFilter = THREE.NearestFilter;
+        canObj.texture.minFilter = THREE.NearestFilter;
+        api.update(canObj);
+        return canObj;
+    };
+    // update
+    api.update = (canObj) => {
+        canObj.draw.call(canObj, canObj, canObj.ctx, canObj.canvas, canObj.state);
+        canObj.texture.needsUpdate = true;
+    };
 }
 
-var currentTriangleIndex = 0;
-const t1 = new THREE.Vector3(0, 0, 0);
-const t2 = new THREE.Vector3(0, 0, 0);
-const t3 = new THREE.Vector3(0, 0, 0);
+setup(canvasMod);
 
-const spotLight = new THREE.SpotLight( 0xff0000 );
-spotLight.position.set( 10, 10, 10 );
-// spotLight.map = new THREE.TextureLoader().load( url );
 
-spotLight.castShadow = true;
+const opt = {
+    size: 64,
+    state: {
+        rPer: 0.2
+    },
+    draw: function (canObj, ctx, canvas, state) {
+        ctx.fillStyle = canObj.palette[1];
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = canObj.palette[0];
+        ctx.beginPath();
+        const hw = canvas.width / 2, sx = hw, sy = canvas.height / 2,
+        radius = hw - hw * state.rPer;
+        ctx.arc(sx, sy, radius, 0, Math.PI * 2);
+        ctx.fill();
+    }
+};
 
-spotLight.shadow.mapSize.width = 1024;
-spotLight.shadow.mapSize.height = 1024;
+var canvas = document.createElement('canvas'),
+ctx = canvas.getContext('2d');
+canvas.width = 8;
+canvas.height = 8;
+ctx.fillStyle = '#000000';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = '#ff00ff';
+ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-spotLight.shadow.camera.near = 500;
-spotLight.shadow.camera.far = 4000;
-spotLight.shadow.camera.fov = 30;
+var texture = new THREE.CanvasTexture(canvas);
+texture.magFilter = THREE.NearestFilter;
+texture.minFilter = THREE.NearestFilter;
 
-scene.add( spotLight );
+// const material = new THREE.MeshBasicMaterial({ map: texture });
+// const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+// const material = new THREE.MeshLambertMaterial({ emissive: new THREE.Color(0xffffff), emissiveMap: texture });
 
-var parts = [];
-var verticesArray = [];
-var indicesArray = [];
+const canvasObject = canvasMod.create(opt);
+const material = new THREE.MeshLambertMaterial({ emissive: new THREE.Color(0xffffff), emissiveMap: canvasObject.texture });
+
+const geo = new THREE.BoxGeometry(10, 10, 10);
+const mesh = new THREE.Mesh( geo, material);
+scene.add(mesh);
