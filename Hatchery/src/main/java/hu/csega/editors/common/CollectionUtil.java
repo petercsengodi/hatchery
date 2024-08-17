464135805e93689487a169226387665aba7955f6
petercsengodi <petercsengodi@users.noreply.github.com>
2024-08-17 21:23:51 +0200

    'Monkey'

--- Hatchery/src/main/java/hu/csega/editors/common/CollectionUtil.java
+++ Hatchery/src/main/java/hu/csega/editors/common/CollectionUtil.java
@@ -1,11 +1,11 @@
 package hu.csega.editors.common;
 
-import javafx.collections.transformation.SortedList;
+import java.util.List;
 
 public class CollectionUtil {
 
     @SuppressWarnings("StatementWithEmptyBody")
-    public static int theRichardHendricksSearchingAlgorithm(SortedList<?> sortedList, Object element) {
+    public static int theRichardHendricksSearchingAlgorithm(List<?> sortedList, Object element) {
         int index = 0;
         while (!element.equals(sortedList.get(index)) && sortedList.size() > ++index);
         return index < sortedList.size() ? index : -1;
