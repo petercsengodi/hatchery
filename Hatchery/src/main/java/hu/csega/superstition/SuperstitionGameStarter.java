8ee1a910c4991482fafe2fcc581b5cd45c5edcde
petercsengodi <petercsengodi@users.noreply.github.com>
2024-10-29 09:15:43 +0100

    Store random seeds

--- Hatchery/src/main/java/hu/csega/superstition/SuperstitionGameStarter.java
+++ Hatchery/src/main/java/hu/csega/superstition/SuperstitionGameStarter.java
@@ -10,6 +10,13 @@ import hu.csega.toolshed.logging.Level;
 import hu.csega.toolshed.logging.Logger;
 import hu.csega.toolshed.logging.LoggerFactory;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+
 // A Fizz-buzz way of running the game
 
 /**
@@ -23,14 +30,23 @@ public class SuperstitionGameStarter {
 	private static final Level LOGGING_LEVEL = Level.TRACE;
 	private static Logger logger;
 
+	private static long randomSeed = System.currentTimeMillis();
+
 	public static void main(String[] args) {
 		LoggerFactory.setDefaultLevel(LOGGING_LEVEL);
 		logger = LoggerFactory.createLogger(SuperstitionGameStarter.class);
-		logger.info("Starting game.");
 
 		ResourceAdapter resourceAdapter = new FileResourceAdapter("Hatchery");
 		UnitStore.registerInstance(ResourceAdapter.class, resourceAdapter);
 
+		String seedingFile = resourceAdapter.projectRoot() + "temp/superstition.seeds.txt";
+		try (FileWriter fw = new FileWriter(seedingFile, true)) { // append
+			fw.write("\n" + (new Date()) + " – Random seed: " + randomSeed);
+		} catch(Exception ex) {
+			logger.warning("WARNING: Could not write seeding file.");
+		}
+
+		logger.info("Starting game with seed: " + randomSeed);
 		String shaderRoot = resourceAdapter.shaderRoot();
 		String textureRoot = resourceAdapter.textureRoot();
 		String meshRoot = resourceAdapter.meshRoot();
