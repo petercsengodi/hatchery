ba81c1f50bf1936dc414c686d3998a1d8fa626ac
petercsengodi <petercsengodi@users.noreply.github.com>
2025-09-06 19:30:34 +0200

    KeepScreen

---
+++ Hatchery/src/main/java/hu/csega/common/robot/KeepScreen.java
@@ -0,0 +1,23 @@
+package hu.csega.common.robot;
+
+import java.awt.*;
+
+public class KeepScreen {
+
+    public static void main(String[] args) throws Exception {
+        Robot robot = new Robot();
+        long startTime = System.currentTimeMillis();
+
+        while (true) {
+            if(System.currentTimeMillis() - startTime > TWENTY_HOURS)
+                break;
+
+            Thread.sleep(10_000L);
+            // robot.mouseMove(0, 0);
+            Point location = MouseInfo.getPointerInfo().getLocation();
+            robot.mouseMove(location.x, location.y);
+        }
+    }
+
+    public static final long TWENTY_HOURS = 20 * 3600 * 1000L;
+}
